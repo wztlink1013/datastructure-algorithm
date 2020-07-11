0502aca46dfc40cd925309841fe488bb366c166f
wztlink1013 <2550374815@qq.com>
2020-07-11 13:25:13 +0800

    update lc509fibonacci

--- src/com/wztlink1013/ds/times/HelloWorld.java
+++ src/com/wztlink1013/ds/times/HelloWorld.java
@@ -1,6 +1,6 @@
-package com.wztlink1013.ds.test;
+package com.wztlink1013.ds.times;
 
-public class HelloWrold {
+public class HelloWorld {
     public static void main(String []args) {
         System.out.println("测试第一个");
     }
