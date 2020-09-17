5f72ac110460de0fa822defc17fc31180acbc12f
wztlink1013 <2550374815@qq.com>
2020-09-17 13:42:02 +0800

    update 子集问题

--- src/com/wztlink1013/al/_递归法_/SubSetting.java
+++ src/com/wztlink1013/al/_递归法_/SubSetting.java
@@ -6,19 +6,26 @@ import java.util.ArrayList;
  * 子集问题
  */
 public class SubSetting {
-    ArrayList<Integer> x  = new ArrayList<Integer>();
+    static ArrayList<Integer> x  = new ArrayList<Integer>();
+    static int cnt = 0;
+
     public static void main(String args[]) {
-        Subsetting(3);
+        int n = 22;
+        Times.test("当n = " + n + "时候的耗费时间", new Times.Task() {
+            public void execute() {
+                Subsetting(n);
+            }
+        });
     }
 
-    private void Subsetting(int n) {
+    private static void Subsetting(int n) {
         if (n > 0) {
             x.add(0);
             Subsetting(n - 1);
-            x.remove(0);
+            x.remove(x.size() - 1);
             x.add(1);
             Subsetting(n - 1);
-            x.remove(n - 1);
+            x.remove(x.size() - 1);
         }else {
             OutputOneSubsetBinary();
             OutputOneSubset();
@@ -26,7 +33,7 @@ public class SubSetting {
         }
     }
 
-    private void OutputOneSubset() {
+    private static void OutputOneSubset() {
         System.out.printf("; {");
         int k = 0;
         for (int i = x.size() - 1; i >=0; i--) {
@@ -40,9 +47,8 @@ public class SubSetting {
         System.out.printf("}");
     }
 
-    private void OutputOneSubsetBinary() {
-        int cnt = 0;
-        System.out.printf("%03d: ", ++cnt);
+    private static void OutputOneSubsetBinary() {
+        System.out.printf("%010d: ", ++cnt);
         for (int i = x.size() - 1; i >= 0; i--)
             System.out.printf("%d", x.get(i));
     }
