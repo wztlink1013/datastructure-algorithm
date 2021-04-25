56c7822a0dacf59c62ab51b14f768d6c5ebc1774
wztlink1013 <2550374815@qq.com>
2021-04-25 10:48:42 +0800

    update

--- src/com/wztlink1013/datastructure/cpp/queue/linkqueue.cpp
+++ src/com/wztlink1013/datastructure/cpp/queue/linkqueue.cpp
@@ -34,11 +34,44 @@ Status InitQueue(LinkQueue &queue) {
 }
 
 /* 入队 */
-
+Status EnQueue(LinkQueue &queue, QElemType e) {
+    QueuePtr temp = new QNode;
+    temp->data = e;
+    temp->next = NULL;
+    queue.rear->next = temp;
+    queue.rear = temp;
+    return OK;
+}
 
 /* 出队 */
-
+Status DeQueue(LinkQueue &queue) {
+    if (queue.front == queue.rear) {
+        return OVERFLOW;
+    }
+    QElemType e = queue.front->next->data;
+    queue.front->next = queue.front->next->next;
+    // 考虑最后一个元素被删，队尾指针指向头结点
+    if (queue.rear == queue.front) {
+        queue.front = queue.rear;
+    }
+    return e;
+}
 
 /* 取队头元素 */
+Status GetHead(LinkQueue &queue) {
+    if (queue.front != queue.rear){
+        return queue.front->next->data;
+    }
+}
 
 
+int main() {
+    LinkQueue test;
+    InitQueue(test);
+    EnQueue(test, 10);
+    EnQueue(test, 15);
+    EnQueue(test, 20);
+    // cout << "队列长度为：" << QueueLength(test) << "\n";
+    cout << GetHead(test)<<" "<< GetHead(test)<<" "<< GetHead(test)<<" "<<"\n";
+    cout << DeQueue(test)<<" "<< DeQueue(test)<<" "<< DeQueue(test)<<" "<<"\n";
+}
